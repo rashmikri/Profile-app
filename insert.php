cd2e57ccd81366803e24b250a7fda9637f75be0e
Rashmi kumari <rashmi1211kumari@gmail.com>
2018-04-26 06:57:25 +0530

    Insertion successful

--- insert.php
+++ insert.php
@@ -1,7 +1,7 @@
 <?php
 include('conn.php');
 
-$user_name = $_POST['user_name'];
+$user_id = $_POST['user_name'];
 $first_name = $_POST['first_name'];
 $last_name = $_POST['last_name'];
 $date_of_birth = $_POST['date_of_birth'];
@@ -11,15 +11,14 @@ $mob_no = $_POST['mob_no'];
 $address = $_POST['address'];
 $password = $_POST['password'];
 
-$sql=("INSERT INTO Registration(user_name, first_name, last_name, date_of_birth, optradio, email, mob_no,address, password) VALUES ($user_name, $first_name, $last_name, $date_of_birth, $optradio, $email, $mob_no, $address, $password)"); 
-$result = mysqli_query($con, $sql);
-if ($result)
-{
-	 echo "Records inserted successfully.";
+$sql="INSERT INTO user(first_name, last_name, date_of_birth, gender, email, mob_no,address, password) VALUES ('$first_name', '$last_name', '$date_of_birth', '$optradio', '$email', '$mob_no', '$address', '$password')"; 
 
+$result = mysqli_query($con, $sql);
+
+if ($result) {
+	echo "Records inserted successfully.";
 } else{
-die('Error: ' . mysqli_error($con));	
+	die('Error: ' . mysqli_error($con));
 }
 
 mysqli_close($con);
-?>
