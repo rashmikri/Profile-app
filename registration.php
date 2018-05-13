ea1adf37a22f034b279ae87e23ffcd37346d7237
Rashmi kumari <rashmi1211kumari@gmail.com>
2018-05-13 19:25:26 +0530

    form added for registeration

--- registration.php
+++ registration.php
@@ -7,58 +7,124 @@
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <script src="js/countrypicker.js"></script>
 </head>
 <body>
-  <div class="container">
-  	<div class="jumbotron">
-  <h2>Sign up</h2><br>
-  <form method="post" action="insert.php">
-  	<div class="form-group">
-      <label for="name">username:</label>
-      <input type="text" name="user_name" class="form-control" id="name" placeholder="Enter userName">
-     </div>
-  	<div class="form-group">
-      <label for="name">First Name:</label>
-      <input type="text" class="form-control" id="name"  name="first_name" placeholder="Enter FName">
-     </div>
-     <div class="form-group">
-      <label for="name">Last Name:</label>
-      <input type="text" name="last_name" class="form-control" id="name" placeholder="Enter LName">
-     </div>
-     <div class="form-group">
-      <label for="dob">Date of Birth:</label>
-      <input type="text" name="date_of_birth" class="form-control" id="dob" placeholder="Enter dob">
-     </div>
-     <h5><B>Gender</B></h5>
-    <label class="radio-inline">
-      <input type="radio" name="optradio" value="male">Male
-    </label>
-    <label class="radio-inline">
-      <input type="radio" name="optradio" value="female">Female
-    </label>
-    <label class="radio-inline">
-      <input type="radio" name="optradio" value="others">Others
-    </label>
-     <div class="form-group">
-      <label for="email">Email id:</label>
-      <input type="email"  name="email" class="form-control" id="email" placeholder="Enter email">
-    </div>
-    <div class="form-group">
-      <label for="name">Mob No:</label>
-      <input type="text" name="mob_no" class="form-control" id="name" placeholder="Enter Mob No">
-     </div>
-    <div class="form-group">
-      <label for="name">Address:</label>
-      <input type="Address" name="address" class="form-control" id="name" placeholder="Enter Address">
-     </div>
-    <div class="form-group">
-      <label for="pwd">Password:</label>
-      <input type="password" class="form-control" id="pwd" placeholder="Enter password" name="password">
-    </div>
-    <button type="submit" class="btn btn-primary">Register</button>
 
-  </form>
-</div>
-</div>
-</body>
-</html>
+  <div class="container">
+  	<div class="form-horizontal">
+    <center> <b> <u><h1>Sign up</h1></b></u><br></center>
+         <form id="reg" method="post" action="insert.php" onSubmit="return formValidation();">
+  	         <div class="form-group">
+              <label for="name">First Name:</label>
+              <input type="text" class="form-control" id="name"  name="first_name" placeholder="Enter FName">
+            </div>
+            <div class="form-group">
+              <label for="name">Last Name:</label>
+              <input type="text" name="last_name"  class="form-control" id="name" placeholder="Enter LName">
+            </div>
+            <div class="form-group">
+              <label for="dob">Date of Birth:</label>
+              <input type="text" name="date_of_birth" class="form-control" id="dob" placeholder="Enter dob">
+            </div>
+            <div class="form-group">
+            <label for="gender">Gender</label>
+            <label class="radio-inline">
+              <input type="radio" name="optradio" value="male" >Male</label>
+              <label class="radio-inline">
+                <input type="radio" name="optradio" value="female" >Female
+              </label>
+              <label class="radio-inline">
+                <input type="radio" name="optradio" value="others" >Others
+              </label><br>
+            </div>
+              <div class="form-group">
+                <label for="name">Mob No:</label>
+                <input type="text" name="mob_no" class="form-control" id="name" placeholder="Enter Mob No">
+              </div>
+              <div class="form-group">
+                <label for="name">Street:</label>
+                 <textarea class="form-control" rows="2" name="street" id="street"></textarea>
+              </div>
+               <div class="row">
+                <div class="col-sm-6">
+                  <div class="form-group">
+                    <label for="city">City</label>
+                    <select class="form-control" id="select" name="city">
+                      <option>Ramgarh</option>
+                      <option>Ranchi</option>
+                      <option>Hazaribagh</option>
+                    </select>
+                  </div>
+                </div>
+                <div class="col-sm-6">
+                  <div class="form-group">
+                    <label for="others">If Others</label>
+                    <input type="others" name="others" class="form-control">
+                  </div>
+              </div>
+            </div>
+             <div class="row">
+                <div class="col-sm-6">
+                  <div class="form-group">
+                    <label for="state">State</label>
+                    <select class="form-control" id="state" name="state">
+                      <option>Jharkhand</option>
+                      <option>Orrisa</option>
+                      <option>Karnataka</option>
+                    </select>
+                  </div>
+                </div>
+                <div class="col-sm-6">
+                  <div class="form-group">
+                    <label for="others">If Others</label>
+                    <input type="others" name="others1" class="form-control">
+                  </div>
+              </div>
+            </div>
+             <div class="row">
+                <div class="col-sm-6">
+                  <div class="form-group">
+                    <label for="pin">Pin Code</label>
+                    <input type="Number" class="form-control" id="pin" name="pin">
+                  </div>
+                </div>
+                <div class="col-sm-6">
+                  <div class="form-group">
+                    <label for="country">Country</label>
+                    <select class="form-control" id="country" name="country">
+                      <option>Afganistan</option>
+                      <option>Albenia</option>
+                      <option>Algeria</option>
+                      <option>India</option>
+                      <option>Indonesia</option>
+                      <option>Bhutan</option>
+                      <option>France</option>
+                      <option>Pakistan</option>
+                    </select>
+                  </div>
+              </div>
+            </div>
+            <div class="form-group">
+              <label for="email">Email Id:</label>
+              <input type="email" name="email" class="form-control" id="email" placeholder="Enter Email">
+            </div>
+            <div class="row">
+              <div class="col-sm-6">
+                <div class="form-group">
+                  <label for="password">Password</label>
+                  <input type="password" name="passw" id="Password" class="form-control">
+                </div>      
+              </div>
+              <div class="col-sm-6">
+                <div class="form-group">
+                  <label for="password">Confirm Password</label>
+                  <input type="password" name="cpassw" id="password" class="form-control">
+                </div> 
+              </div>
+            </div>
+          <center>  <button type="submit" class="btn btn-primary btn-lg">Submit</button></center>
+
+
+        </body>
+        </html>
