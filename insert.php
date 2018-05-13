683f737d46a47fcfc2c605eb969b69d00678968c
Rashmi kumari <rashmi1211kumari@gmail.com>
2018-05-13 20:19:59 +0530

    insering data into database

--- insert.php
+++ insert.php
@@ -1,22 +1,32 @@
 <?php
 include('conn.php');
 
-$user_id = $_POST['user_name'];
 $first_name = $_POST['first_name'];
 $last_name = $_POST['last_name'];
 $date_of_birth = $_POST['date_of_birth'];
 $optradio = $_POST['optradio'];
-$email = $_POST['email'];
 $mob_no = $_POST['mob_no'];
-$address = $_POST['address'];
-$password = $_POST['password'];
+$street = $_POST['street'];
+$city = $_POST['city'];
+$others = $_POST['others'];
+$state = $_POST['state'];
+$others1 = $_POST['others1'];
+$pin = $_POST['pin'];
+$country = $_POST['country'];
+$email = $_POST['email'];
+$passw = $_POST['passw'];
+$cpassw = $_POST['cpassw'];
+$passw = md5($passw);
+$cpassw = md5($cpassw);
 
-$sql="INSERT INTO user(first_name, last_name, date_of_birth, gender, email, mob_no,address, password) VALUES ('$first_name', '$last_name', '$date_of_birth', '$optradio', '$email', '$mob_no', '$address', '$password')"; 
+
+$sql="INSERT INTO user(first_name, last_name, date_of_birth, optradio, mob_no, street,city,others ,others1,state,pin,country,email, passw,cpassw) VALUES ('$first_name', '$last_name', '$date_of_birth', '$optradio', '$mob_no', '$street','$city','$others','$state','$others1','$pin','$country','$email','$passw', '$cpassw')"; 
 
 $result = mysqli_query($con, $sql);
 
 if ($result) {
-	echo "Records inserted successfully.";
+	echo '<script>alert("Registeration done")</script>';
+	echo '<p>Continue to <a href="login.php"><Button type="submit" class="btn btn-primary">Login</Button></a><p>';
 } else{
 	die('Error: ' . mysqli_error($con));
 }
